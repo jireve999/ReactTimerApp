98518bf6c6d8116cd599d940c0c4dd91a42385b3
SAJI <wbajxw999@aliyun.com>
2016-06-05 15:48:31 +0800

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+
+var Countdown = React.createClass({
+  render:function(){
+    return(
+      <p>Countdown.jsx render</p>
+    )
+  }
+});
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+
+var Timer = React.createClass({
+  render:function(){
+    return(
+      <p>Timer.jsx render</p>
+    )
+  }
+});
+
+module.exports = Timer;
